f6f1fcd5193308c024eeb0937ae49f60cc30efcd
DoxSolutions25 <admin@businesselitesoft.com>
2024-11-27 11:45:36 +0200

    2 Commit

---
+++ doxbook_theme/doctype/branch_users/branch_users.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Nesscale and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class BranchUsers(Document):
+	pass
---
+++ doxbook_theme/doctype/permission_check/test_permission_check.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Nesscale and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPermissionCheck(FrappeTestCase):
+	pass
